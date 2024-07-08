a9212e2a68fe5a9feb5eefaf54641e706c39bf31
Harry Santoso <harrysantoso8@yahoo.com>
2024-07-09 00:00:39 +0700

    fix currency yen

--- src/store/modules/curr/index.js
+++ src/store/modules/curr/index.js
@@ -1,62 +1,70 @@
-import mutations from './mutations'
-import getters from './getters'
-import actions from './actions'
+import mutations from "./mutations";
+import getters from "./getters";
+import actions from "./actions";
 
 export default {
-    state() {
-        return {
-            list: [{
-                "name": "Indonesian Rupiah",
-                "code": "IDR",
-                // "icon": "rupiah.png"
-            },
-            {
-                "name": "Malaysian Ringgit",
-                "code": "MYR",
-                // "icon": "ringgit.png"
-            },
-            {
-                "name": "New Zealand Dollar",
-                "code": "NZD",
-                // "icon": "nzd.png"
-            },
-            {
-                "name": "Hongkong Dollar",
-                "code": "HKD",
-                // "icon": "hkd.png"
-            }, {
-                "name": "Great Britain Pound",
-                "code": "GBP",
-                // "icon": "gbp.png"
-            }, {
-                "name": "Euro",
-                "code": "EUR",
-                // "icon": "euro.png"
-            }, {
-                "name": "Thailand Baht",
-                "code": "THB",
-                // "icon": "baht.png"
-            }, {
-                "name": "Australian Dollar",
-                "code": "AUD",
-                // "icon": "aud.png"
-            }, {
-                "name": "Chinese Yuan Renminbi",
-                "code": "CNY",
-                // "icon": "yuan.png"
-            }, {
-                "name": "Japanese Yen",
-                "code": "YEN",
-                // "icon": "yen.png"
-            },
-            {
-                "name": "US Dollar",
-                "code": "USD",
-                // "icon": "usd.png"
-            }]
-        }
-    },
-    mutations,
-    getters,
-    actions
-}
+  state() {
+    return {
+      list: [
+        {
+          name: "Indonesian Rupiah",
+          code: "IDR",
+          // "icon": "rupiah.png"
+        },
+        {
+          name: "Malaysian Ringgit",
+          code: "MYR",
+          // "icon": "ringgit.png"
+        },
+        {
+          name: "New Zealand Dollar",
+          code: "NZD",
+          // "icon": "nzd.png"
+        },
+        {
+          name: "Hongkong Dollar",
+          code: "HKD",
+          // "icon": "hkd.png"
+        },
+        {
+          name: "Great Britain Pound",
+          code: "GBP",
+          // "icon": "gbp.png"
+        },
+        {
+          name: "Euro",
+          code: "EUR",
+          // "icon": "euro.png"
+        },
+        {
+          name: "Thailand Baht",
+          code: "THB",
+          // "icon": "baht.png"
+        },
+        {
+          name: "Australian Dollar",
+          code: "AUD",
+          // "icon": "aud.png"
+        },
+        {
+          name: "Chinese Yuan Renminbi",
+          code: "CNY",
+          // "icon": "yuan.png"
+        },
+        {
+          name: "Japanese Yen",
+          code: "JPY",
+          // "icon": "yen.png"
+        },
+        {
+          name: "US Dollar",
+          code: "USD",
+          // "icon": "usd.png"
+        },
+      ],
+    };
+  },
+  mutations,
+  getters,
+  actions,
+};
